16aa056e6b09366186dc99609cf54a45e424b799
Bob Beachill <bbeachill@icloud.com>
2025-01-26 21:55:05 +0000

    Complteed the one button challenge using constants and if statement

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,32 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message: String = "I Am A Programmer!"
+    @State private var message: String = ""
+    @State private var imageString: String = ""
+    
     var body: some View {
         
         VStack {
             Spacer()
-            Image(systemName: "swift")
+            Image(systemName: imageString)
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-                .frame(width: 200, height: 200)
+            
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.ultraLight)
 //                .foregroundStyle(.red)
             Spacer()
             
-            HStack {
-                Button("Awesome") {
-                    message = "Awesome!"
-                }
+            Button("Press Me!") {
+                let message1 = "You are Awesome!"
+                let message2 = "You are Great!"
+                let imageString1 = "hand.thumbsup"
+                let imageString2 = "sun.max.fill"
                 
-                Button("Great") {
-                    message = "Great!"
+                if message == message1 {
+                    message = message2
+                    imageString = imageString2
+                } else {
+                    message = message1
+                    imageString = imageString1
                 }
-        
             }
+                
             .buttonStyle(.borderedProminent)
             .font(.title2)
             .tint(.orange)
