9d55bf64ec045e8e878b09fed7410e7816875290
Bob Beachill <bbeachill@icloud.com>
2025-01-27 18:05:41 +0000

    Added several images which are cycled through upon clicking button

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var message: String = ""
     @State private var imageName: String = ""
+    @State private var imageNumber = 0
     
     var body: some View {
         
@@ -32,11 +33,14 @@ struct ContentView: View {
             Button("Press Me!") {
                 let message1 = "You are Awesome!"
                 let message2 = "You are Great!"
-//                let imageName1 = "image0"
-//                let imageName2 = "image1"
                 
+                imageName = "image" + String(imageNumber)
                 message = message == message1 ? message2 : message1
-                imageName = imageName == "image0" ? "image1" : "image0"
+                
+                imageNumber += 1
+                if imageNumber > 9 {
+                    imageNumber = 0
+                }
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
