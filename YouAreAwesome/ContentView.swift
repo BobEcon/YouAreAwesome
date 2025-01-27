f418986267f7febc2ecddee65e3c8c40292ce82f
Bob Beachill <bbeachill@icloud.com>
2025-01-27 00:00:58 +0000

    Added some images to assets catalogue and used a couple in place of system images

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,43 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var message: String = ""
-    @State private var imageString: String = ""
+    @State private var imageName: String = ""
     
     var body: some View {
         
         VStack {
             Spacer()
-            Image(systemName: imageString)
+            
+            Image(imageName)
                 .resizable()
                 .scaledToFit()
-                .foregroundStyle(.orange)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
             
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.ultraLight)
-//                .foregroundStyle(.red)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+
             Spacer()
             
             Button("Press Me!") {
                 let message1 = "You are Awesome!"
                 let message2 = "You are Great!"
-                let imageString1 = "hand.thumbsup"
-                let imageString2 = "sun.max.fill"
+//                let imageName1 = "image0"
+//                let imageName2 = "image1"
                 
-//                if message == message1 {
-//                    message = message2
-//                    imageString = imageString2
-//                } else {
-//                    message = message1
-//                    imageString = imageString1
-//                }
                 message = message == message1 ? message2 : message1
-                imageString = imageString == imageString1 ? imageString2 : imageString1
+                imageName = imageName == "image0" ? "image1" : "image0"
             }
-                
             .buttonStyle(.borderedProminent)
             .font(.title2)
-            .tint(.orange)
         }
         .padding()
         
