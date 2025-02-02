67e718e44c4756428fcca05763a1f61f5e0e1972
Bob Beachill <bbeachill@icloud.com>
2025-02-02 23:26:49 +0000

    added toggle for sound on/off, stopped sounds overlapping each toher

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
     @State private var lastMessageNumber = -1
     @State private var lastSoundNumber: Int = -1
     @State private var audioPlayer: AVAudioPlayer!
+    @State private var soundIsOn = true
     let numberOfImages: Int = 10
     let numberOfSounds: Int = 6
     
@@ -42,27 +43,45 @@ struct ContentView: View {
 
             Spacer()
             
-            Button("Show Message") {
-                let messages = ["You are Awesome!",
-                                "When the Genius Bar Needs help, They Call You!",
-                                "You are Great!",
-                                "You are Fantastic!",
-                                "Fabulous? That's You!",
-                                "You Make Me smile!",
-                                ]
+            HStack {
+                Text("Sound On:")
+                Toggle("", isOn: $soundIsOn)
+                    .labelsHidden()
+                    .onChange(of: soundIsOn) {
+                        if audioPlayer != nil && audioPlayer.isPlaying {
+                            audioPlayer.stop()
+                        }
+                    }
                 
-                lastMessageNumber = nonRepeatingRandom(lastNumber: lastMessageNumber, upperBound: messages.count-1)
-                message = messages[lastMessageNumber]
+                Spacer()
                 
-                lastImageNumber = nonRepeatingRandom(lastNumber: lastImageNumber, upperBound: numberOfImages-1)
-                imageName = "image" + String(lastImageNumber)
-                
-                lastSoundNumber = nonRepeatingRandom(lastNumber: lastSoundNumber, upperBound: numberOfSounds-1)
-                
-                playSound(soundName: "sound" + String(lastSoundNumber))
+                Button("Show Message") {
+                    let messages = ["You are Awesome!",
+                                    "When the Genius Bar Needs help, They Call You!",
+                                    "You are Great!",
+                                    "You are Fantastic!",
+                                    "Fabulous? That's You!",
+                                    "You Make Me smile!",
+                    ]
+// It works here too
+//                    if audioPlayer != nil && audioPlayer.isPlaying {
+//                        audioPlayer.stop()
+//                    }
+                    lastMessageNumber = nonRepeatingRandom(lastNumber: lastMessageNumber, upperBound: messages.count-1)
+                    message = messages[lastMessageNumber]
+                    
+                    lastImageNumber = nonRepeatingRandom(lastNumber: lastImageNumber, upperBound: numberOfImages-1)
+                    imageName = "image" + String(lastImageNumber)
+                    
+                    lastSoundNumber = nonRepeatingRandom(lastNumber: lastSoundNumber, upperBound: numberOfSounds-1)
+                    if soundIsOn {
+                        playSound(soundName: "sound" + String(lastSoundNumber))
+                    }
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                .font(.title2)
             }
-            .buttonStyle(.borderedProminent)
-            .font(.title2)
         }
         .padding()
         
@@ -77,6 +96,9 @@ struct ContentView: View {
     }
     
     func playSound(soundName: String) {
+        if audioPlayer != nil && audioPlayer.isPlaying {
+            audioPlayer.stop()
+        }
         guard let soundFile = NSDataAsset(name: soundName) else
         {
             print("😡 Could not read file named \(soundName)")
